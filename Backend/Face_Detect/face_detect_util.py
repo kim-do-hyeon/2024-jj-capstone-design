7d210562824fe07a9c3f2c49dbb7c9c65c7462dc
pental <pental@kakao.com>
2024-02-09 17:32:49 +0900

    [Update] code refactoring

--- Backend/Face_Detect/face_detect_util.py
+++ Backend/Face_Detect/face_detect_util.py
@@ -1,63 +1,48 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-import dlib
 import cv2
-
+import numpy as np
+import matplotlib.pyplot as plt
 import face_recognition
 
 def plt_imshow(title='image', img=None, figsize=(8 ,5)):
     plt.figure(figsize=figsize)
- 
     if type(img) == list:
         if type(title) == list:
             titles = title
         else:
             titles = []
- 
             for i in range(len(img)):
                 titles.append(title)
- 
         for i in range(len(img)):
             if len(img[i].shape) <= 2:
                 rgbImg = cv2.cvtColor(img[i], cv2.COLOR_GRAY2RGB)
             else:
                 rgbImg = cv2.cvtColor(img[i], cv2.COLOR_BGR2RGB)
- 
             plt.subplot(1, len(img), i + 1), plt.imshow(rgbImg)
             plt.title(titles[i])
             plt.xticks([]), plt.yticks([])
- 
         plt.show()
     else:
         if len(img.shape) < 3:
             rgbImg = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         else:
             rgbImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
- 
         plt.imshow(rgbImg)
         plt.title(title)
         plt.xticks([]), plt.yticks([])
         plt.show()
 
-
 def name_labeling(input_image, known_face_encodings, known_face_names):
     image = input_image.copy()
     face_locations = face_recognition.face_locations(image)
     face_encodings = face_recognition.face_encodings(image, face_locations)
-    
     face_names = []
- 
     for face_encoding in face_encodings:
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
         name = "Unknown"
- 
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
- 
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
- 
         face_names.append(name)
         
     for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -65,13 +50,12 @@ def name_labeling(input_image, known_face_encodings, known_face_names):
             color = (0, 255, 0)
         else:
             color = (0, 0, 255)
- 
         cv2.rectangle(image, (left, top), (right, bottom), color, 1)
         cv2.rectangle(image, (left, bottom - 10), (right, bottom), color, cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(image, name, (left + 3, bottom - 3), font, 0.2, (0, 0, 0), 1)
         
-    plt_imshow("Output", image, figsize=(24, 15))
+    # plt_imshow("Output", image, figsize=(24, 15))
     return name
 
 def draw_label(input_image, coordinates, label):
@@ -79,16 +63,13 @@ def draw_label(input_image, coordinates, label):
     (top, right, bottom, left) = coordinates
     cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 5)
     cv2.putText(image, label, (left - 10, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-    
     return image
     
 def add_known_face(face_image_path, name, known_face_encodings, known_face_names):
     face_image = cv2.imread(face_image_path)
     face_location = face_recognition.face_locations(face_image)[0]
     face_encoding = face_recognition.face_encodings(face_image)[0]
-    
     detected_face_image = draw_label(face_image, face_location, name)
-    
     known_face_encodings.append(face_encoding)
     known_face_names.append(name)
     print(">> ADD KNOWN FACE ENCODING >> {}".format(face_image_path))
