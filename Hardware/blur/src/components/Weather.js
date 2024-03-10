c0153e1402b22e3c9768ba7386f026e29ba4a10a
Rachel <cofla226@naver.com>
2024-03-10 23:26:18 +0900

    [Hardware][Add] add weather component with real-time weather API integration

---
+++ Hardware/blur/src/components/Weather.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Weather.css'
+
+import { 
+    WiDaySunny, 
+    WiDayCloudyHigh, 
+    WiCloudy, 
+    WiCloud,
+    WiShowers,
+    WiRain,
+    WiThunderstorm,
+    WiSnowflakeCold,
+    WiFog
+} from "react-icons/wi";
+
+function Weather() {
+    const [weather, setWeather] = useState("");
+    const [temperature, setTemperature] = useState("");
+    const [city, setCity] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [iconCode, setIconCode] = useState(""); // 아이콘 상태 추가
+
+    // 날씨 아이콘 반환 함수
+    const getWeatherIcon = (iconCode) => {
+        switch (iconCode) {
+            case '01n': return <WiDaySunny className="weatherIcon" />;
+            case '02n': return <WiDayCloudyHigh className="weatherIcon" />;
+            case '03n': return <WiCloudy className="weatherIcon" />;
+            case '04n': return <WiCloud className="weatherIcon" />;
+            case '09n': return <WiShowers className="weatherIcon" />;
+            case '10n': return <WiRain className="weatherIcon" />;
+            case '11n': return <WiThunderstorm className="weatherIcon" />;
+            case '13n': return <WiSnowflakeCold className="weatherIcon" />;
+            case '50n': return <WiFog className="weatherIcon" />;
+            default: return null; // 혹은 기본 날씨 아이콘
+        }
+    };
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            try {
+                navigator.geolocation.getCurrentPosition(async (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const API_KEY = process.env.BLUR_APP_OPEN_WEATHER_API_KEY;
+                    const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
+                    
+                    console.log(response.data); // 전체 응답 로깅
+                    console.log("Icon code from API:", response.data.current.weather[0].icon); // 아이콘 코드 로깅
+        
+                    setWeather(response.data.current.weather[0].description);
+                    setTemperature(response.data.current.temp);
+                    setIconCode(response.data.current.weather[0].icon); // 아이콘 코드 상태 설정
+
+                    // 현재 위치의 도시명 가져오기
+                    const cityResponse = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`);
+                    setCity(cityResponse.data.address.city);
+            
+                    setLoading(false);
+                });
+            } catch (error) {
+                console.error('Error fetching weather:', error);
+                setLoading(false);
+            }
+        };
+    
+        fetchWeather();
+    }, []);
+
+    return (
+        <div className="weather">
+            {loading ? (
+                "날씨 정보 가져오는 중..."
+            ) : (
+                <div>
+                <div>{getWeatherIcon(iconCode)}</div>
+                <div className="tempInfo">{parseFloat(temperature).toFixed(1)}°C</div>
+                <div className="weatherInfo">{weather}</div>
+                <div className="cityInfo">{city}</div>
+                </div>
+            )}
+        </div>
+    );
+}
+    
+    export default Weather;
