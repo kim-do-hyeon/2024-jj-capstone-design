2a3b85885683ac1327106d9acdd7862ba73dc1c0
Rachel <cofla226@naver.com>
2024-03-10 23:25:28 +0900

    [Hardware][Add] add cheerup component with dynamic messages

---
+++ Hardware/blur/src/components/CheerUp.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './CheerUp.css';
+
+function CheerUp() {
+    const [cheerUp, setCheerUp] = useState('');
+
+    const cheerUps = [
+        '멋진 날이 될 거예요!', 
+        '좋은 하루 보내세요!', 
+        '오늘은 어땠나요?', 
+        '다 잘 될 거예요!'
+    ];
+
+    const getRandomIndex = (length) => {
+        return Math.floor(Math.random() * length);
+    };
+
+    useEffect(() => {
+        setCheerUp(cheerUps[getRandomIndex(cheerUps.length)]);
+    }, []);
+
+    return (
+        <div className="welcomeSign">
+        반가워요,
+        <div className='userName'>
+            Guest.
+        </div>
+        {cheerUp}
+        </div>
+    );
+}
+
+export default CheerUp;
