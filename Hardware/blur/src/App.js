560aad22c44299740a38ee60c3099c00ccb8f0a3
Rachel <cofla226@naver.com>
2024-03-23 02:13:11 +0900

    [Hardware][Update] implement widget fetching from server and display in console

--- Hardware/blur/src/App.js
+++ Hardware/blur/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import DateTime from './components/DateTime';
 import Weather from './components/Weather';
@@ -8,6 +9,21 @@ import Login from './components/Login';
 
 
 function App() {
+  const [widgets, setWidgets] = useState(null);
+
+  useEffect(() => {
+    const fetchWidgets = async () => {
+      try {
+        const response = await axios.get('https://jj.system32.kr/widgets');
+        console.log(response.data);
+        setWidgets(response.data);
+      } catch (error) {
+        console.error('Error fetching widgets:', error);
+      }
+    };
+
+    fetchWidgets();
+  }, []);
 
   return (
     <div className="App">
@@ -33,7 +49,6 @@ function App() {
       <div className='Weather'>
         <Weather /> 
       </div>
-   
   </div>
   );
 }
