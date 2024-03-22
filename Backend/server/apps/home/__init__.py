d0576d3917dde038e3cfd5ee7ca96c1f319d7060
Rachel <cofla226@naver.com>
2024-03-23 02:04:33 +0900

    [Backend][Add] add CORS policy support

--- Backend/server/apps/home/__init__.py
+++ Backend/server/apps/home/__init__.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from flask_cors import CORS
 
 blueprint = Blueprint(
     'home_blueprint',
     __name__,
     url_prefix=''
 )
+
+CORS(blueprint, resources={r"/*": {"origins": "http://localhost:3000"}})
