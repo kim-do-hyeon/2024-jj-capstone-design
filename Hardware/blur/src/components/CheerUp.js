9dd6c54b0bd38cfdc4e329b502d10634b22c9c8c
pental <pental@kakao.com>
2024-04-05 00:58:56 +0900

    [Hardware][Feat] add face detect functions

--- Hardware/blur/src/components/CheerUp.js
+++ Hardware/blur/src/components/CheerUp.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './CheerUp.css';
 
-function CheerUp() {
+function CheerUp({ face }) {
     const [cheerUp, setCheerUp] = useState('');
 
     const cheerUps = [
@@ -16,22 +16,26 @@ function CheerUp() {
     };
 
     useEffect(() => {
-        const timer = setTimeout ( () => {
-            setCheerUp(cheerUps[getRandomIndex(cheerUps.length)]);    
-        }, 4000); //cheerUp 텍스트 시간 차 두고 렌더링
+        const timer = setTimeout(() => {
+            if (face === "Unknown") {
+                setCheerUp("반가워요, Guest.");
+            } else {
+                setCheerUp(cheerUps[getRandomIndex(cheerUps.length)]);
+            }
+        }, 4000); // cheerUp 텍스트 시간 차 두고 렌더링
 
         return () => clearTimeout(timer);
-    }, []);
+    }, [face]); // face 값이 변할 때마다 useEffect 다시 실행
 
     return (
         <div className="welcomeSign">
-        <span className='textAnimation'>반가워요,</span>
-        <div className='userName'>
-            Guest.
-        </div>
-        <div>
-        {cheerUp}
-        </div>
+            <span className='textAnimation'>반가워요,</span>
+            <div className='userName'>
+                {face === "Unknown" ? "Guest" : face}.
+            </div>
+            <div>
+                {cheerUp}
+            </div>
         </div>
     );
 }
