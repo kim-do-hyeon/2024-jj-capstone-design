2ca886012412c842afc7754d23267122e704247b
pental <pental@kakao.com>
2024-05-07 23:18:46 +0900

    [Backend][Refactor] modules

--- Backend/server/apps/home/face_module.py
+++ Backend/server/apps/home/face_module.py
@@ -1,3 +1,7 @@
+# -*- encoding: utf-8 -*-
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+from apps.home.personal_color_moudle import analysis
 
 ''' Import DB '''
 from apps import db
@@ -21,3 +25,84 @@ def predict_face(image) :
     except :
         pass
 
+def redirect_face_module() :
+    face_image = request.files['face_image']
+    upload_dir = "upload/predict/"
+    
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    filename = secure_filename(face_image.filename)
+    if len(filename.split(".")) == 1 :
+        filename = "noname." + filename.split(".")[-1]
+    file_path = os.path.join(upload_dir, filename)
+    try:
+        face_image.save(file_path)
+    except Exception as e:
+        return jsonify(result = "fail", type = "save_image", message = str(e))
+    
+    result = (predict_face(file_path))
+    try :
+        username = Faces.query.filter_by(displayname = result).first()
+        username = (username.username)
+    except :
+        username = ""
+    return jsonify(result = "success", type = "face", face = str(result), username = username)
+
+def distance_module() :
+    focal_length = 950
+    known_width = 18.0
+    face_width_pixels = 200
+
+    def distance_to_camera(known_width, focal_length, face_width):
+        return (known_width * focal_length) / face_width
+    
+    distance_image = request.files['distance_image']
+    upload_dir = "upload/distance/"
+    
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    filename = secure_filename(distance_image.filename)
+
+    if len(filename.split(".")) == 1 :
+        filename = "noname." + filename.split(".")[-1]
+
+    file_path = os.path.join(upload_dir, filename)
+
+    try:
+        distance_image.save(file_path)
+    except Exception as e:
+        return jsonify(result = "fail", type = "save_image", message = str(e))
+    
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    distance_image = cv2.imread(file_path)
+    gray = cv2.cvtColor(distance_image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    result_distance = 0
+    for (x, y, w, h) in faces:
+        result_distance = distance_to_camera(known_width, focal_length, w)
+
+    if result_distance == 0 :
+        return jsonify(result = "fail", type = "distance", distance = 0)
+    else :
+        return jsonify(result = "success", type = "distance", distance = result_distance)
+
+def personal_color_module() :
+    face_image = request.files['face_image']
+    upload_dir = "upload/personal/"
+    
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    filename = secure_filename(face_image.filename)
+
+    if len(filename.split(".")) == 1 :
+        filename = "noname." + filename.split(".")[-1]
+
+    file_path = os.path.join(upload_dir, filename)
+
+    try:
+        face_image.save(file_path)
+    except Exception as e:
+        return jsonify(result = "fail", type = "save_image", message = str(e))
+    
+    tone = analysis(file_path)
+    return jsonify(result = "success", type = "perosnal_color", tone = tone)
---
+++ Backend/server/apps/home/widgets_module.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+import json
+from flask import request, jsonify, session
+
+''' Import DB '''
+from apps import db
+from apps.authentication.models import Widget, CustomLocation, Production
+
+def widgets_module() :
+    widget_names = {}
+    widget_list = Widget.query.all()
+    for i in widget_list :
+        widget_names[i.id] = i.widget_name
+    return jsonify(result = "success", type = "widget_list", message = widget_names)
+
+def widgets_custom_module() :
+    data = request.args.to_dict()
+    location = data['index']
+    model_code = data['model_code']
+    try :
+        if (CustomLocation.query.filter_by(username = session['username'], model_code = str(model_code)).first()) == None :
+            new_data = CustomLocation(username = session['username'], model_code = str(model_code), index = str(location))
+            db.session.add(new_data)
+            db.session.commit()
+            return jsonify(result = "success", type = "widget_custom", message = "New Widget")
+        else :
+            CustomLocation.query.filter_by(username = session['username'], model_code = str(model_code)).update(dict(index = str(location)))
+            db.session.commit()
+            return jsonify(result = "success", type = "widget_custom", message = "Update Widget")
+    except Exception as e :
+        return jsonify(result = "fail", type = "widget_custom", message = "Fail : {}".format(str(e)))
+
+def get_widgets_custom_module(subpath) :
+    data = subpath.split("=")
+    username = data[-1]
+    # model_code = data['model_code']
+    model_code = "1234-5678" # Temporary Code
+    user_data = CustomLocation.query.filter_by(username = username, model_code = str(model_code)).first()
+    if user_data != None :
+        user_widgets = json.loads(user_data.index)
+        return jsonify(result = "success", type = "get_widgets_custom", message = user_widgets)
+    else :
+        return jsonify(result = "fail", type = "get_widgets_custom", message = "Empty Database")
+
+def get_model_user_list_module() :
+    data = request.args.to_dict()
+    model_code = data['code']
+    user_list = Production.query.filter_by(code = model_code).all()
+    user_names = []
+    for i in user_list :
+        user_names.append(i.username)
+    if len(user_list) != 0 :
+        return jsonify(result = "success", type = "model_user_list", message = str(user_names))
+    else :
+        return jsonify(result = "success", type = "model_user_list", message = "Empty Model User List")
+
+def get_guset_widgets_index_module() :
+    return jsonify(result = "success",
+                   message = {'Weather' : [1, 1],
+                              'DateTime' : [1, 2],
+                              'Login' : [1, 3],
+                              'CheerUp' : [1, 4]})
