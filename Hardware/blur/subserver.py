4c8da5af6e69f9f1e9aecad97b557e2db0144304
pental <pental@kakao.com>
2024-05-09 00:27:30 +0900

    [Hardware][Fix] message values & font-size, text-align

--- Hardware/blur/subserver.py
+++ Hardware/blur/subserver.py
@@ -12,11 +12,37 @@ def get_kospi():
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     kospi = soup.select_one('#content > div.article > div.section2 > div.section_stock_market > div.section_stock > div.kospi_area.group_quot').text
-    kospi = (list(kospi.split("\n")))[5]
+    kospi = list(kospi.split("\n"))
+    kospi_value = kospi[5]
+    kospi_change_value = kospi[6]
+    kospi_change_rate = kospi[7]
+    if kospi[8] == "상승" :
+        kospi_type = "▲"
+    else :
+        kospi_type = "▼"
     kosdaq = soup.select_one('#content > div.article > div.section2 > div.section_stock_market > div.section_stock > div.kosdaq_area.group_quot').text
-    kosdaq = (list(kosdaq.split("\n")))[5]
+    kosdaq = list(kosdaq.split("\n"))
+    kosdaq_value = kosdaq[5]
+    kosdaq_change_value = kosdaq[6]
+    kosdaq_change_rate = kosdaq[7]
+    if kosdaq[8] == "상승" :
+        kosdaq_type = "▲"
+    else :
+        kosdaq_type = "▼"
+    
     rate = soup.select_one("#content > div.article2 > div.section1 > div.group1 > table > tbody > tr.up.bold > td:nth-child(2)").text
-    return jsonify(kospi = kospi, kosdaq = kosdaq, rate = rate)
+    rate_change = soup.select_one("#content > div.article2 > div.section1 > div.group1 > table > tbody > tr.up.bold > td:nth-child(3)").text
+    rate_change_value = rate_change.split(" ")[-1]
+    if "상승" in rate_change :
+        rate_type = "▲"
+    else :
+        rate_type = "▼"
+
+    kospi_message = "{} {}  {}({})".format(kospi_type, kospi_value, kospi_change_value, kospi_change_rate)
+    kosdaq_message = "{} {}  {}({})".format(kosdaq_type, kosdaq_value, kosdaq_change_value, kosdaq_change_rate)
+    rate_message = "{} {}  ({})".format(rate_type, rate, rate_change_value)
+    
+    return jsonify(kospi = kospi_message, kosdaq = kosdaq_message, rate = rate_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
