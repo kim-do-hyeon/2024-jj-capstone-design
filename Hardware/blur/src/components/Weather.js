9107f7ecef16f9ec38a94b151ababa43370fec25
Ggksmf <158995019+Ggksmf@users.noreply.github.com>
2024-06-05 21:00:58 +0900

    [Hardware][Feat] RainStopMessages, ContinuousRainMessages, RainStartMessages

--- Hardware/blur/src/components/Weather.js
+++ Hardware/blur/src/components/Weather.js
@@ -21,16 +21,31 @@ function Weather() {
     const [temperature, setTemperature] = useState("");
     const [city, setCity] = useState("");
     const [loading, setLoading] = useState(true);
-    const [iconCode, setIconCode] = useState(""); // 아이콘 상태 추가
-    const [showUmbrellaMessage, setShowUmbrellaMessage] = useState("");
+    const [iconCode, setIconCode] = useState("");
     const [showTempDifferenceMessage, setShowTempDifferenceMessage] = useState("");
     const [showHotMessage, setShowHotMessage] = useState("");
-    const umbrellaMessages = [
-        '비가 올 것 같으니 우산을 챙기세요!',
-        '비가 오니 우산을 꼭 챙기세요!',
-        '오늘은 우산이 필요할 것 같아요.',
-        '우산을 챙기지 않으면 비에 맞을 수 있으니 주의하세요.',
-        '옷을 젖게 하지 않기 위해 우산을 꼭 챙기세요.',
+    const [showRainStopMessage, setShowRainStopMessage] = useState("");
+    const [showContinuousRainMessage, setShowContinuousRainMessage] = useState("");
+    const [showRainStartMessage, setShowRainStartMessage] = useState("");
+    const  rainStopMessages= [
+        '비가 그치고 있어서 다행이네요.',
+        '비가 멈추고 있으니 산책하기 좋은 날씨네요.',
+        '우산을 접어놓으셔도 될 것 같아요.',
+        '비가 그치니 늦은 저녁 산책 어떠세요?',
+        '비가 그치고 하늘이 맑아질 거에요.'
+    ];
+
+    const  continuousRainMessages= [
+        '비가 계속 오고 있으니 우산을 챙기세요.',
+        '비가 계속 내리고 있어서 실내에서 따뜻한 차 한 잔 어떠세요?',
+        '비가 계속 오는데 우산을 꼭 챙기세요.',
+        '비가 계속 오는데 감기 조심하세요.'
+    ];
+
+    const  rainStartMessages= [
+        '비가 올 것 같습니다! 우산을 꼭 챙기세요.',
+        '비가 살짝 내리기 시작했습니다. 우산을 챙기는 걸 잊지 마세요!',
+        '비가 살짝씩 올 것 같아요. 우산을 챙기는 게 좋겠어요.',
     ];
 
     const tempDifferenceMessages = [
@@ -54,7 +69,6 @@ function Weather() {
         '날씨가 더워도 에너지는 열정적으로 유지하세요!'
     ];
 
-    // 날씨 아이콘 반환 함수
     const getWeatherIcon = (iconCode) => {
         switch (iconCode) {
             case '01n': return <WiDaySunny className="weatherIcon" />;
@@ -69,14 +83,13 @@ function Weather() {
             case '11n': return <WiThunderstorm className="weatherIcon" />;
             case '13n': return <WiSnowflakeCold className="weatherIcon" />;
             case '50n': return <WiFog className="weatherIcon" />;
-            default: return null; // 혹은 기본 날씨 아이콘
+            default: return null;
         }
     };
 
     useEffect(() => {
         const fetchWeather = async () => {
             try {
-                // IP 주소를 통해 위치 정보 가져오기
                 const locationResponse = await axios.get('https://ipapi.co/json/');
                 const { latitude, longitude, city } = locationResponse.data;
                 setLocation({ latitude, longitude });
@@ -84,42 +97,53 @@ function Weather() {
 
                 const API_KEY = 'e4699688bda8af6d121b61b33727cbe4';
                 const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
-                console.log(response.data); // 전체 응답 로깅
-                console.log("Icon code from API:", response.data.current.weather[0].icon); // 아이콘 코드 로깅
 
                 setWeather(response.data.current.weather[0].description);
                 setTemperature(response.data.current.temp);
-                setIconCode(response.data.current.weather[0].icon); // 아이콘 코드 상태 설정
+                setIconCode(response.data.current.weather[0].icon);
 
-                // 최고 기온과 최저 기온을 가져와서 일교차를 계산
                 const dailyData = response.data.daily;
                 const maxTemp = dailyData[0].temp.max;
                 const minTemp = dailyData[0].temp.min;
                 const tempDifference = maxTemp - minTemp;
             
-                // 일교차가 10도 이상인 경우에는 옷을 챙기라는 메시지를 표시
                 if (tempDifference >= 10) {
                     setShowTempDifferenceMessage(true);
                 } else {
                     setShowTempDifferenceMessage(false);
                 }
-                
-                // 비가 오는 경우에는 우산을 챙기라는 메시지를 표시
-                const currentWeather = response.data.current.weather[0].main.toLowerCase();
-                const rainForecast = response.data.hourly.slice(1, 6).some(hourlyData => hourlyData.weather[0].main.toLowerCase().includes('rain'));
-                if (currentWeather.includes('rain') || rainForecast) {
-                    setShowUmbrellaMessage(true);
-                } else {
-                    setShowUmbrellaMessage(false);
-                }
 
-                // 온도가 30도 이상일 때 "오늘 너무 덥습니다" 메시지를 표시
                 if (response.data.current.temp >= 30) {
                     setShowHotMessage(true);
                 } else {
                     setShowHotMessage(false);
                 }
-        
+
+                // 이전 날씨 상태를 저장할 변수
+                let previousWeather = "";
+
+                // 현재 날씨 상태
+                const currentWeather = response.data.current.weather[0].main.toLowerCase();
+
+                // 1시간 뒤 날씨 상태
+                const nextHourWeather = response.data.hourly[1].weather[0].main.toLowerCase();
+
+                // 이전 날씨 상태가 없는 경우 현재 날씨 상태를 이전 상태로 설정
+                if (!previousWeather) {
+                    previousWeather = currentWeather;
+                } else {
+                    // 이전 날씨 상태와 현재 날씨 상태가 다른 경우에 메시지 표시
+                    if (currentWeather.includes('rain') && !previousWeather.includes('rain')) {
+                        setShowRainStartMessage(true); // 비가 오다가 시작하는 경우 메시지 표시
+                    } else if (!currentWeather.includes('rain') && previousWeather.includes('rain')) {
+                        setShowRainStopMessage(true); // 비가 오다가 멈추는 경우 메시지 표시
+                    } else if (currentWeather.includes('rain') && previousWeather.includes('rain')) {
+                        setShowContinuousRainMessage(true); // 비가 계속 오는 경우 메시지 표시
+                    }
+                    // 현재 날씨 상태를 이전 상태로 설정
+                    previousWeather = currentWeather;
+                }
+
                 setLoading(false);
             } catch (error) {
                 console.error('Error fetching weather:', error);
@@ -130,9 +154,17 @@ function Weather() {
         fetchWeather();
     }, [setLocation]);
 
-    const getRandomUmbrellaMessage = () => {
-        const randomIndex = Math.floor(Math.random() * umbrellaMessages.length);
-        return umbrellaMessages[randomIndex];
+    const getRandomRainStopMessage = () => {
+        const randomIndex = Math.floor(Math.random() * rainStopMessages.length);
+        return rainStopMessages[randomIndex];
+    };
+    const getRandomContinuousRainMessage = () => {
+        const randomIndex = Math.floor(Math.random() * continuousRainMessages.length);
+        return continuousRainMessages[randomIndex];
+    };
+    const getRandomRainStartMessage = () => {
+        const randomIndex = Math.floor(Math.random() * rainStartMessages.length);
+        return rainStartMessages[randomIndex];
     };
     const getRandomTempDifferenceMessage = () => {
         const randomIndex = Math.floor(Math.random() * tempDifferenceMessages.length);
@@ -157,16 +189,21 @@ function Weather() {
                     </div>
                 <div className="weatherInfo">{weather}</div>
                 <div className="cityInfo">{city}</div>
-                    {showUmbrellaMessage && (
-                        <div className="messageInfo">{getRandomUmbrellaMessage()}</div>
-                    )}
                     {showTempDifferenceMessage && (
                         <div className="messageInfo">{getRandomTempDifferenceMessage()}</div>
                     )}
                     {showHotMessage && (
                         <div className="messageInfo">{getRandomHotMessage()}</div>
                     )}
-
+                    {showRainStartMessage && (
+                        <div className="messageInfo">{getRandomRainStopMessage()}</div>
+                    )}
+                    {showContinuousRainMessage && (
+                        <div className="messageInfo">{getRandomContinuousRainMessage()}</div>
+                    )}
+                    {showRainStartMessage && (
+                        <div className="messageInfo">{getRandomRainStartMessage()}</div>
+                    )}
                 </div>
             )}
         </div>
