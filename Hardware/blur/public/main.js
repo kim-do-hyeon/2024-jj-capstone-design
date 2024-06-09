82dc43b22997ea2e106f0c3f7706ad6a902ce6e7
JUNO <tlswnsgh0624@naver.com>
2024-06-09 20:17:56 +0900

    [Hardware][Feat] elecron fullscreen with no menu bar

--- Hardware/blur/public/main.js
+++ Hardware/blur/public/main.js
@@ -7,6 +7,8 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        fullscreen: true,
+        frame: false,
         webPreferences: {
             enableRemoteModule: true
         }
