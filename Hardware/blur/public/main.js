ba9b780dbb01eb9d8c02365b2893427acec100db
tlswnsgh0624 <tlswnsgh0624@naver.com>
2024-06-14 05:18:05 +0900

    [Hardware][Feat] seperate electron development env. & production env.

--- Hardware/blur/public/main.js
+++ Hardware/blur/public/main.js
@@ -1,7 +1,7 @@
-const {app, BrowserWindow} = require('electron');
-
-const remote = require('@electron/remote/main')
-remote.initialize()
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const remote = require('@electron/remote/main');
+remote.initialize();
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -12,21 +12,25 @@ function createWindow() {
         webPreferences: {
             enableRemoteModule: true
         }
-    })
+    });
 
-    win.loadURL('http://localhost:3000')
+    if (process.env.NODE_ENV === 'development') {
+        win.loadURL('http://localhost:3000');
+    } else {
+        win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+    }
 
     remote.enable(win.webContents);
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function() {
-    if(process.platform !== 'darwin') {
-        app.quit() 
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
-})
+});
 
 app.on('activate', function() {
-    if(BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
