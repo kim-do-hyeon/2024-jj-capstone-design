41c68d17f887e31f217b122594cf5c956cbe888b
Rachel <cofla226@naver.com>
2024-06-17 22:43:52 +0900

    [Hardware][Update] modify message widget functionality

--- Hardware/blur/src/components/Message.js
+++ Hardware/blur/src/components/Message.js
@@ -3,38 +3,54 @@ import axios from 'axios';
 import './Message.css';
 
 function Message({ userName }) {
-  const [latestMessage, setLatestMessage] = useState('');
-  const [sender, setSender] = useState('');
+  const [unreadMessages, setUnreadMessages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-    const fetchMessage = async () => {
-      const url = `https://jj.system32.kr/get_messages/${userName}`;
+    const fetchUnreadMessages = async () => {
+      const url = `https://jj.system32.kr/get_unread_messages/${userName}`;
       try {
         const response = await axios.get(url);
         if (response.data.result === 'success' && response.data.messages.length > 0) {
-          const lastMessageIndex = response.data.messages.length - 1;
-          const lastMessage = response.data.messages[lastMessageIndex];
-          setLatestMessage(lastMessage.content);
-          setSender(lastMessage.sender);
+          setUnreadMessages(response.data.messages);
+          
+          await axios.post(`https://jj.system32.kr/mark_messages_as_read/${userName}`);
         } else {
-          console.error('No messages found or failed to retrieve messages.');
-          setLatestMessage("");
-          setSender('');
+          console.error('No unread messages found or failed to retrieve messages.');
+          setUnreadMessages([]);
         }
       } catch (error) {
-        console.error('Error fetching messages:', error);
-        setLatestMessage("");
-        setSender('');
+        console.error('Error fetching unread messages:', error);
+        setUnreadMessages([]);
       }
     };
 
-    fetchMessage();
+    fetchUnreadMessages();
   }, [userName]);
+
+  useEffect(() => {
+    if (unreadMessages.length > 0) {
+      const interval = setInterval(() => {
+        setCurrentPage(prevPage => (prevPage + 1) % Math.ceil(unreadMessages.length / 3));
+      }, 10000);
+      return () => clearInterval(interval);
+    }
+  }, [unreadMessages]);
+
+  const startIndex = currentPage * 3;
+  const currentMessages = unreadMessages.slice(startIndex, startIndex + 3);
 
   return (
     <div className="message">
-      {latestMessage && <p><strong>From :</strong> <span>{sender}</span> - <span>{latestMessage}</span></p>}
-      {!latestMessage && <p>No messages available.</p>}
+      {currentMessages.length > 0 ? (
+        currentMessages.map((message, index) => (
+          <p key={index}>
+            <strong>From :</strong> <span>{message.sender}</span> - <span>{message.content}</span>
+          </p>
+        ))
+      ) : (
+        <p>No unread messages available.</p>
+      )}
     </div>
   );
 }
