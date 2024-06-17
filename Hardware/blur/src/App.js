793d2f10f70a330197e554846eaaac30de7b16bd
Rachel <cofla226@naver.com>
2024-06-18 01:08:19 +0900

    [Hardware][Feat] display black screen when no user is detected

--- Hardware/blur/src/App.js
+++ Hardware/blur/src/App.js
@@ -6,10 +6,11 @@ import Weather from './components/Weather';
 import CheerUp from './components/CheerUp';
 import Login from './components/Login';
 import Finance from './components/Finance';
-import News from './components/News'
+import News from './components/News';
 import Message from './components/Message';
-import Traffic from './components/Traffic'
+import Traffic from './components/Traffic';
 import DHT from './components/DHT';
+import Start from './components/Start';
 import axios from 'axios';
 
 function App() {
@@ -18,12 +19,12 @@ function App() {
     const [userName, setUserName] = useState("Guest");
     const [userid, setUserId] = useState("Guest");
     const [isActive, setIsActive] = useState(false);
+    const [showStart, setShowStart] = useState(false);
 
     useEffect(() => {
         const fetchWidgets = async () => {
             try {
                 const response = await axios.get('https://jj.system32.kr/widgets_index');
-                // const response = await axios.get('https://jj.system32.kr/get_widgets_custom/username=qqq');
                 const messageOnly = response.data.message;
                 const mappedWidgets = Object.entries(messageOnly).map(([type, position]) => ({
                     type,
@@ -45,62 +46,79 @@ function App() {
         setIsActive(active);
         setUserName(name);
         setUserId(id);
-        try {
-            const loginResponse = await axios.post('https://jj.system32.kr/get_widgets_custom/username=' + id);
-            if(loginResponse.data.message === "Empty Database"){
-                try {
-                    const response = await axios.get('https://jj.system32.kr/widgets_index');
-                    const messageOnly = response.data.message;
-                    const mappedWidgets = Object.entries(messageOnly).map(([type, position]) => ({
+        if (active) {
+            if (!showStart) {
+                setShowStart(true);
+                setTimeout(() => setShowStart(false), 1000);
+            }
+            try {
+                const loginResponse = await axios.post('https://jj.system32.kr/get_widgets_custom/username=' + id);
+                if (loginResponse.data.message === "Empty Database") {
+                    try {
+                        const response = await axios.get('https://jj.system32.kr/widgets_index');
+                        const messageOnly = response.data.message;
+                        const mappedWidgets = Object.entries(messageOnly).map(([type, position]) => ({
+                            type,
+                            row: position[0],
+                            col: position[1]
+                        }));
+                        setWidgets(mappedWidgets);
+                    } catch (error) {
+                        console.error('Error fetching widgets:', error);
+                    }
+                } else {
+                    const mappedWidgets = Object.entries(loginResponse.data.message).map(([type, position]) => ({
                         type,
                         row: position[0],
                         col: position[1]
                     }));
                     setWidgets(mappedWidgets);
-                } catch (error) {
-                    console.error('Error fetching widgets:', error);
                 }
-            }else{
-                const mappedWidgets = Object.entries(loginResponse.data.message).map(([type, position]) => ({
-                    type,
-                    row: position[0],
-                    col: position[1]
-                }));
-                console.log(mappedWidgets);
-                setWidgets(mappedWidgets);
+            } catch (error) {
+                console.error('Error logging in:', error);
             }
-        } catch (error) {
-            console.error('Error logging in:', error);
+        } else {
+            setWidgets([]);
         }
     };
 
     return (
         <div className={`container ${showText ? 'show' : ''}`}>
-            {[1, 2, 3].map(row => (
-                <div className="row" key={row} style={{ height: "330px" }}>
-                    {[1, 2, 3, 4].map(col => (
-                        <div className="col" key={col}>
-                            {widgets.find(widget => widget.row === row && widget.col === col) ? (
-                                (() => {
-                                    const widget = widgets.find(widget => widget.row === row && widget.col === col);
-                                    switch (widget.type) {
-                                        case 'DateTime': return <DateTime key={`${row}-${col}`} />;
-                                        case 'Weather': return <Weather key={`${row}-${col}`} />;
-                                        case 'CheerUp': return <CheerUp key={`${row}-${col}`} userName={userName} />;
-                                        case 'Login': return <Login key={`${row}-${col}`} />;
-                                        case 'Finance' : return <Finance key={`${row}-${col}`} />;
-                                        case 'News' : return <News key={`${row}-${col}`} />;
-                                        case 'Traffic' : return <Traffic key={`${row}-${col}`} />;
-                                        case 'Message' : return <Message key={`${row}-${col}`} userName={userName}/>;
-                                        case 'Room' : return <DHT key={`${row}-${col}`} />;
-                                        default: return null;
-                                    }
-                                })()
-                            ) : null}
-                        </div>
-                    ))}
-                </div>
-            ))}
+            {showStart ? (
+                <Start userName={userName} />
+            ) : (
+                <>
+                    {!isActive ? (
+                        <div className="black-screen"></div>
+                    ) : (
+                        [1, 2, 3].map(row => (
+                            <div className="row" key={row} style={{ height: "330px" }}>
+                                {[1, 2, 3, 4].map(col => (
+                                    <div className="col" key={col}>
+                                        {widgets.find(widget => widget.row === row && widget.col === col) ? (
+                                            (() => {
+                                                const widget = widgets.find(widget => widget.row === row && widget.col === col);
+                                                switch (widget.type) {
+                                                    case 'DateTime': return <DateTime key={`${row}-${col}`} />;
+                                                    case 'Weather': return <Weather key={`${row}-${col}`} />;
+                                                    case 'CheerUp': return <CheerUp key={`${row}-${col}`} userName={userName} />;
+                                                    case 'Login': return <Login key={`${row}-${col}`} />;
+                                                    case 'Finance': return <Finance key={`${row}-${col}`} />;
+                                                    case 'News': return <News key={`${row}-${col}`} />;
+                                                    case 'Traffic': return <Traffic key={`${row}-${col}`} />;
+                                                    case 'Message': return <Message key={`${row}-${col}`} userName={userName} />;
+                                                    case 'Room': return <DHT key={`${row}-${col}`} />;
+                                                    default: return null;
+                                                }
+                                            })()
+                                        ) : null}
+                                    </div>
+                                ))}
+                            </div>
+                        ))
+                    )}
+                </>
+            )}
             <CameraFeed onUserDetected={handleUserDetection} />
         </div>
     );
--- Hardware/blur/src/index.js
+++ Hardware/blur/src/index.js
@@ -3,29 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { WeatherProvider } from './components/WeatherContext';
 import App from './App';
-import Start from './components/Start';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const startAndSwitch = () => {
-    root.render(
-        <React.StrictMode>
-            <Start />
-        </React.StrictMode>
-    );
-    setTimeout(() => {
-        root.render(
-            <React.StrictMode>
-                <WeatherProvider>
-                    <App />
-                </WeatherProvider>
-            </React.StrictMode>
-        );
-    }, 2000);
-};
-
-startAndSwitch();
+root.render(
+    <React.StrictMode>
+        <WeatherProvider>
+            <App />
+        </WeatherProvider>
+    </React.StrictMode>
+);
 
 reportWebVitals();
