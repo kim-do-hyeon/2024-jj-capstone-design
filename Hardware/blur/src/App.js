193d4dd71f045ba7ce58d8013fb853b12233570b
Ggksmf <158995019+Ggksmf@users.noreply.github.com>
2024-06-18 17:03:52 +0900

    [Hardware][Feat] Deleted content

--- Hardware/blur/src/App.js
+++ Hardware/blur/src/App.js
@@ -11,6 +11,7 @@ import Message from './components/Message';
 import Traffic from './components/Traffic';
 import DHT from './components/DHT';
 import PersonalColor from './components/PersonalColor';
+import Start from './components/Start';
 import axios from 'axios';
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
     const [userName, setUserName] = useState("Guest");
     const [userid, setUserId] = useState("Guest");
     const [isActive, setIsActive] = useState(false);
+    const [showStart, setShowStart] = useState(false);
+    const [lastUserId, setLastUserId] = useState("Guest");
+    const [userLostTimeout, setUserLostTimeout] = useState(null);
 
     useEffect(() => {
         const fetchWidgets = async () => {
@@ -42,66 +46,100 @@ function App() {
     }, []);
 
     const handleUserDetection = async (active, name = "Guest", id = "Guest") => {
-        setIsActive(active);
-        setUserName(name);
-        setUserId(id);
-        try {
-            const loginResponse = await axios.post('https://jj.system32.kr/get_widgets_custom/username=' + id);
-            if(loginResponse.data.message === "Empty Database"){
+        if (active) {
+            if (id !== lastUserId) {
+                setUserName(name);
+                setUserId(id);
+                setLastUserId(id);
+                setShowStart(true);
                 try {
-                    const response = await axios.get('https://jj.system32.kr/widgets_index');
-                    const messageOnly = response.data.message;
-                    const mappedWidgets = Object.entries(messageOnly).map(([type, position]) => ({
-                        type,
-                        row: position[0],
-                        col: position[1]
-                    }));
-                    setWidgets(mappedWidgets);
+                    const loginResponse = await axios.post('https://jj.system32.kr/get_widgets_custom/username=' + id);
+                    if (loginResponse.data.message === "Empty Database") {
+                        try {
+                            const response = await axios.get('https://jj.system32.kr/widgets_index');
+                            const messageOnly = response.data.message;
+                            const mappedWidgets = Object.entries(messageOnly).map(([type, position]) => ({
+                                type,
+                                row: position[0],
+                                col: position[1]
+                            }));
+                            setWidgets(mappedWidgets);
+                        } catch (error) {
+                            console.error('Error fetching widgets:', error);
+                        }
+                    } else {
+                        const mappedWidgets = Object.entries(loginResponse.data.message).map(([type, position]) => ({
+                            type,
+                            row: position[0],
+                            col: position[1]
+                        }));
+                        setWidgets(mappedWidgets);
+                    }
                 } catch (error) {
-                    console.error('Error fetching widgets:', error);
+                    console.error('Error logging in:', error);
+                } finally {
+                    setTimeout(() => setShowStart(false), 1000);
                 }
             } else {
-                const mappedWidgets = Object.entries(loginResponse.data.message).map(([type, position]) => ({
-                    type,
-                    row: position[0],
-                    col: position[1]
-                }));
-                console.log(mappedWidgets);
-                setWidgets(mappedWidgets);
+                setShowStart(false);
             }
-        } catch (error) {
-            console.error('Error logging in:', error);
+            setIsActive(true);
+            if (userLostTimeout) {
+                clearTimeout(userLostTimeout);
+                setUserLostTimeout(null);
+            }
+        } else {
+            if (!userLostTimeout) {
+                const timeout = setTimeout(() => {
+                    setIsActive(false);
+                    setUserName("Guest");
+                    setUserId("Guest");
+                    setWidgets([]);
+                    setUserLostTimeout(null);
+                }, 5000);
+                setUserLostTimeout(timeout);
+            }
         }
     };
 
     return (
         <div className={`container ${showText ? 'show' : ''}`}>
-            {[1, 2, 3].map(row => (
-                <div className="row" key={row} style={{ height: "330px" }}>
-                    {[1, 2, 3, 4].map(col => (
-                        <div className="col" key={col}>
-                            {widgets.find(widget => widget.row === row && widget.col === col) ? (
-                                (() => {
-                                    const widget = widgets.find(widget => widget.row === row && widget.col === col);
-                                    switch (widget.type) {
-                                        case 'DateTime': return <DateTime key={`${row}-${col}`} />;
-                                        case 'Weather': return <Weather key={`${row}-${col}`} />;
-                                        case 'CheerUp': return <CheerUp key={`${row}-${col}`} userName={userName} />;
-                                        case 'Login': return <Login key={`${row}-${col}`} />;
-                                        case 'Finance' : return <Finance key={`${row}-${col}`} />;
-                                        case 'News' : return <News key={`${row}-${col}`} />;
-                                        case 'Traffic' : return <Traffic key={`${row}-${col}`} />;
-                                        case 'Message' : return <Message key={`${row}-${col}`} userName={userName}/>;
-                                        case 'Room' : return <DHT key={`${row}-${col}`} />;
-                                        case 'PersonalColor': return <PersonalColor key={`${row}-${col}`} />;
-                                        default: return null;
-                                    }
-                                })()
-                            ) : null}
-                        </div>
-                    ))}
-                </div>
-            ))}
+            {showStart ? (
+                <Start userName={userName} />
+            ) : (
+                <>
+                    {!isActive ? (
+                        <div className="black-screen"></div>
+                    ) : (
+                        [1, 2, 3].map(row => (
+                            <div className="row" key={row} style={{ height: "330px" }}>
+                                {[1, 2, 3, 4].map(col => (
+                                    <div className="col" key={col}>
+                                        {widgets.find(widget => widget.row === row && widget.col === col) ? (
+                                            (() => {
+                                                const widget = widgets.find(widget => widget.row === row && widget.col === col);
+                                                switch (widget.type) {
+                                                    case 'DateTime': return <DateTime key={`${row}-${col}`} />;
+                                                    case 'Weather': return <Weather key={`${row}-${col}`} />;
+                                                    case 'CheerUp': return <CheerUp key={`${row}-${col}`} userName={userName} />;
+                                                    case 'Login': return <Login key={`${row}-${col}`} />;
+                                                    case 'Finance': return <Finance key={`${row}-${col}`} />;
+                                                    case 'News': return <News key={`${row}-${col}`} />;
+                                                    case 'Traffic': return <Traffic key={`${row}-${col}`} />;
+                                                    case 'Message': return <Message key={`${row}-${col}`} userName={userName} />;
+                                                    case 'Room': return <DHT key={`${row}-${col}`} />;
+                                                    case 'PersonalColor': return <PersonalColor key={`${row}-${col}`} />;
+                                                    default: return null;
+                                                }
+                                            })()
+                                        ) : null}
+                                    </div>
+                                ))}
+                            </div>
+                        ))
+                    )}
+                </>
+            )}
             <CameraFeed onUserDetected={handleUserDetection} />
         </div>
     );
