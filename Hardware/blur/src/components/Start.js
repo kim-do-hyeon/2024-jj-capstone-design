88ff071fc8d7db8d913088699d78d9b6781a8028
Rachel <cofla226@naver.com>
2024-06-19 15:11:18 +0900

    [Hardware][Update] update welcome message

--- Hardware/blur/src/components/Start.js
+++ Hardware/blur/src/components/Start.js
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './Start.css';
+import { clear } from '@testing-library/user-event/dist/clear';
 
-function Start({ userName }) {
+function Start() {
     const [fadeOut, setFadeOut] = useState(false);
     const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
+    useEffect( () => {
+        const timeout = setTimeout( () => {
             setFadeOut(true);
         }, 1000);
 
         return () => clearTimeout(timeout);
     }, []);
 
-    let greeting = ''; // 인사말 저장할 변수 초기화
+    let greeting = '';//인사말 저장할 변수 초기화
 
     const hour = currentDateTime.getHours();
 
-    // 시간에 따른 다른 시작 텍스트 출력
+    //시간에 따른 다른 시작 텍스트 출력
     if (hour >= 5 && hour < 11) {
         greeting = '활기찬 아침 보내세요!';
     } else if (hour >= 12 && hour < 18) {
@@ -25,12 +26,10 @@ function Start({ userName }) {
     } else {
         greeting = '느긋한 밤이네요 :)';
     }
-
     return (
         <div className={`startContainer ${fadeOut ? 'fadeOut' : ''}`}>
             <div className='centeredText'>{greeting}</div>
         </div>
     );
 }
-
 export default Start;
